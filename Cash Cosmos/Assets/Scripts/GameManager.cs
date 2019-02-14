1fdac11d846124e57b0df4bc3315d8aa2b32a682
Storm <stormmayer@gmail.com>
2019-02-14 16:50:29 -0500

    Basic Components
    There isn't much, but I put in some of the basics.

---
+++ Cash Cosmos/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private float currency;
+    private float modifier;
+    private bool mod1;
+    private bool mod2;
+
+	// Use this for initialization
+	void Start () {
+        currency = 0;
+        modifier = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Calc Modifier
+    float calcModifier()
+    {
+        float total = 1.0f;
+        return total;
+    }
+
+    //Check modifier upgrades
+    void checkUpgrades()
+    {
+
+    }
+}
