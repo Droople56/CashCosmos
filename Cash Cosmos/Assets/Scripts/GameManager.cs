203f3682998433f5b9609c037933006c64b5ddda
Mitch Steffens <mdsteffens1@gmail.com>
2019-02-14 19:55:35 -0500

    MVI Done
    Basic UI, 2 Upgrades, Basic Resource Harvesting
    Code needs some organization and clean-up.

--- Cash Cosmos/Assets/Scripts/GameManager.cs
+++ Cash Cosmos/Assets/Scripts/GameManager.cs
@@ -1,23 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-    private float currency;
+    public float currency;
     private float modifier;
     private bool mod1;
     private bool mod2;
+
+    private float lumberPrice;
+    float lumberUpgradePrice;
+
+    public float treeGrowthFactor;
+    float fertilizerCost;
+    int fertilizerAmount;
+
+    const string LUMBER_STR = "Lumber Price + $";
+
+    Text currencyText;
+    Text lumberText;
+    Button lumberUpgradeButton;
+    Button fertilizerUpgradeButton;
 
 	// Use this for initialization
 	void Start () {
         currency = 0;
         modifier = 1.0f;
-	}
+
+        currencyText = GameObject.Find("CurrencyText").GetComponent<Text>();
+
+
+        //tree price and upgrades
+        lumberPrice = 1.0f;
+        lumberUpgradePrice = 10.0f;
+        treeGrowthFactor = 1.0f;
+
+        fertilizerAmount = 1;
+        fertilizerCost = Mathf.Pow(fertilizerAmount, 3) * 200;
+
+        lumberText = GameObject.Find("LumberText").GetComponent<Text>();
+        lumberUpgradeButton = GameObject.Find("LumberUpgradeButton").GetComponent<Button>();
+        lumberUpgradeButton.transform.GetChild(0).GetComponent<Text>().text = LUMBER_STR + 1;
+        lumberUpgradeButton.transform.GetChild(1).GetComponent<Text>().text = "Cost: $" + lumberUpgradePrice;
+
+        fertilizerUpgradeButton = GameObject.Find("FertilizerUpgradeButton").GetComponent<Button>();
+        fertilizerUpgradeButton.transform.GetChild(0).GetComponent<Text>().text = "Double Tree Growth";
+        fertilizerUpgradeButton.transform.GetChild(1).GetComponent<Text>().text = "Cost: $" + fertilizerCost;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        currencyText.text = "Monies: $" + currency;
 	}
 
     //Calc Modifier
@@ -31,5 +66,37 @@ public class GameManager : MonoBehaviour {
     void checkUpgrades()
     {
 
+    }
+
+    public void SellLumber()
+    {
+        currency += lumberPrice;
+    }
+
+    //when the lumber price upgraded
+    public void IncreaseLumberPrice(float amount)
+    {
+        if (currency >= lumberUpgradePrice)
+        {
+            currency -= lumberUpgradePrice;
+            lumberPrice += amount;
+        }
+
+        lumberUpgradePrice = Mathf.Pow(lumberPrice, 2) * 10;
+        lumberUpgradeButton.transform.GetChild(1).GetComponent<Text>().text = "Cost: $" + lumberUpgradePrice;
+        lumberText.text = "Lumber Price: $" + lumberPrice;
+    }
+
+    public void IncreaseFertilizer(float amount)
+    {
+        if(currency >= fertilizerCost)
+        {
+            currency -= fertilizerCost;
+            treeGrowthFactor *= 2.0f;
+        }
+
+        fertilizerAmount++;
+        fertilizerCost =  Mathf.Pow(fertilizerAmount, 3) * 200;
+        fertilizerUpgradeButton.transform.GetChild(1).GetComponent<Text>().text = "Cost: $" + fertilizerCost;
     }
 }
---
+++ Cash Cosmos/Assets/Scripts/TreeScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour {
+
+    const float GROW_TIME = 4.0f;
+    bool growing;
+    float currentTime;
+
+    GameManager mngr;
+
+	// Use this for initialization
+	void Start () {
+        growing = false;
+        currentTime = 0.0f; //used to reset the trees once they're cut down
+
+        mngr = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CutDown();
+	}
+
+    float timer;
+
+    public void CutDown()
+    {
+        if (growing)
+        {
+            if (currentTime < GROW_TIME / mngr.treeGrowthFactor)
+            { 
+                if (GetComponent<SpriteRenderer>().enabled == true)
+                {
+                    GetComponent<SpriteRenderer>().enabled = false;
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+                currentTime += Time.fixedDeltaTime;
+            }
+            else
+            {
+                growing = false;
+                currentTime = 0.0f;
+                GetComponent<SpriteRenderer>().enabled = true;
+                GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        growing = true;
+        mngr.SellLumber();
+    }
+}
