cba95fc6faa6c9d6c2f07e278ef824eb1b70e95a
Alec Walker <axw1799@rit.edu>
2019-03-05 16:40:36 -0500

    feedback when player clicks a planet. changes size of planet for a sec, and gradually gets smaller closer to death

--- Cash Cosmos/Assets/Scripts/TreeScript.cs
+++ Cash Cosmos/Assets/Scripts/TreeScript.cs
@@ -9,57 +9,86 @@ public class TreeScript : MonoBehaviour {
     public int planetHealth;
     public float speedMod;
     private float planetScale;
-
+    bool giveFeedback;
+    private Vector2 planetFeedbackScale;
+    private int scaleTimer;
     GameManager mngr;
 
 	// Use this for initialization
 	void Start () {
-
+        
+        scaleTimer = 0;
+        giveFeedback = false;
         switch (planetNumber)
         {
             case 1:
                 planetValue = 1;
                 speedMod = Random.Range(0.8f, 1.5f);
                 planetHealth = 9;
-                planetScale = Random.Range(0.3f, 0.6f);
+                planetScale = Random.Range(0.5f, 0.7f);
                 break;
             case 2:
                 planetValue = 10;
                 speedMod = Random.Range(1.5f, 1.9f);
                 planetHealth = 14;
-                planetScale = Random.Range(0.3f, 0.6f);
+                planetScale = Random.Range(0.5f, 0.7f);
                 break;
             case 3:
                 planetValue = 50;
                 speedMod = Random.Range(2.0f, 2.5f);
                 planetHealth = 19;
-                planetScale = Random.Range(0.3f, 0.6f);
+                planetScale = Random.Range(0.5f, 0.7f);
                 break;
             case 4:
                 planetValue = -10;
                 speedMod = Random.Range(0.8f, 1.5f);
                 planetHealth = 4;
-                planetScale = Random.Range(0.3f, 0.6f);
+                planetScale = Random.Range(0.5f, 0.7f);
                 break;
             default:
                 break;
         }
 
         this.transform.localScale *= planetScale;
-
+        planetFeedbackScale = transform.localScale;
         mngr = GameObject.Find("GameManager").GetComponent<GameManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-	}
+        clickFeedback();
+    }
 
     private void OnMouseDown()
     {
         planetHealth--;
+        giveFeedback = true;
         finalClick();
         mngr.SellResource(planetValue);
+    }
+
+    void clickFeedback()
+    {
+        if (giveFeedback)
+        {
+            if (scaleTimer <= 4)
+            {
+                planetFeedbackScale.x += .005f;
+                planetFeedbackScale.y += .005f;
+            }
+            else if(scaleTimer>=5 && scaleTimer < 10)
+            {
+                planetFeedbackScale.x -= .0051f;
+                planetFeedbackScale.y -= .0051f;
+            }
+            else
+            {
+                scaleTimer = 0;
+                giveFeedback = false;
+            }
+            scaleTimer++;
+            transform.localScale = planetFeedbackScale;
+        }
     }
 
     private void finalClick()
