ad669a94ff32814386c18ac2456fe60460668499
Mitch Steffens <mdsteffens1@gmail.com>
2019-04-28 22:27:18 -0400

    Some Interface Changes
    Fixed some broken sizing garbage (probably temporarily)

--- Cash Cosmos/Assets/Scripts/ScrollScript.cs
+++ Cash Cosmos/Assets/Scripts/ScrollScript.cs
@@ -10,6 +10,9 @@ public class ScrollScript : MonoBehaviour {
     Vector3 originalPos;
     public float maxDist;
 
+    [Tooltip("0: Right to Left\n1:Left to Right\n2:Top to Bottom\n3:Bottom to Top")]
+    public int direction = 0;
+
 	// Use this for initialization
 	void Start () {
         originalPos = buttons.transform.position;
@@ -17,6 +20,28 @@ public class ScrollScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        buttons.transform.position = originalPos + new Vector3(scroller.value * maxDist,0);
-	}
+
+        Vector3 moveVector = Vector3.zero;
+        switch (direction)
+        {
+            case 0:
+                moveVector = new Vector3(scroller.value * maxDist, 0);
+                break;
+            case 1:
+                moveVector = new Vector3(-scroller.value * maxDist, 0);
+                break;
+            case 2:
+                moveVector = new Vector3(0, scroller.value * maxDist);
+                break;
+            case 3:
+                moveVector = new Vector3(0, -scroller.value * maxDist);
+                break;
+            default:
+                Debug.Log("Must choose a value between 1 and 3 for direction");
+                break;
+        }
+        buttons.transform.position = originalPos + moveVector;
+            
+    }
+      
 }
---
+++ Cash Cosmos/Assets/Scripts/UnlockTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockTile : MonoBehaviour
+{
+    Text description;
+
+    public float cost = 100.0f;
+    public float yield = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        description = this.transform.GetChild(1).GetComponent<Text>();
+        description.text = "Cost: " + cost + "\nDestruction Profit: " + yield;
+    }
+
+}
